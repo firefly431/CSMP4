be27d2dacf337683a0c6bfe7cad7023cf4243c89
thiensonho <thiensonho@gmail.com>
2014-12-08 13:23:50 -0600

    Bullet Class!

---
+++ cs/spaceship/entity/Bullet.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs.spaceship.entity;
+
+import cs.geom.AABB;
+import cs.geom.Vector2D;
+import cs.spaceship.GameFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author s544545
+ */
+public class Bullet extends Entity {
+    public int radius;
+    public double velx, vely;
+    public double posx, posy;
+    public Color color;
+
+    public Bullet() {
+        position = new Vector2D();
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setVelocity(double x, double y) {
+        this.velx = x;
+        this.vely = y;
+    }
+
+    @Override
+    public void update() {
+        posx += velx;
+        posy += vely;
+        position.set((int)posx, (int)posy);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval((int)posx - radius, (int)posy - radius, radius * 2, radius * 2);
+        g.setColor(Color.WHITE);
+        g.fillOval((int)posx - radius * 4 / 5, (int)posy - radius * 4 / 5, radius * 4 / 5 * 2, radius * 4 / 5 * 2);
+    }
+
+    @Override
+    public boolean shouldDelete() {
+        return !(new AABB(GameFrame.WINDOW_WIDTH, GameFrame.WINDOW_HEIGHT).intersects(getAABB()));
+    }
+
+    public int COLLISION_WIDTH() {
+        return radius * 2;
+    }
+
+    public int COLLISION_HEIGHT() {
+        return radius * 2;
+    }
+}
